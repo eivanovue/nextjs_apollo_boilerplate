a6d6372c820840d08fba0d4548cd2cc67332ed13
Emil Ivanov <emil@eivanovue.com>
2021-11-22 15:51:41 +0000

    Some funky updates to look and feel

--- packages/graphql/index.ts
+++ packages/graphql/index.ts
@@ -1,14 +1,30 @@
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
   type Query {
-    hello: String
+      books: [Book]
   }
 `;
 
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  }
+];
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello !',
+    books: () => books,
   },
 };
 
